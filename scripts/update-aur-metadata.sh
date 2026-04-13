e3bdd688e3e0c14d4722bfcd60514ffef383ae13
OneNoted <notes@madeingotland.com>
2026-04-13 20:54:27 +0200

    fix: generate srcinfo as non-root in CI

--- scripts/update-aur-metadata.sh
+++ scripts/update-aur-metadata.sh
@@ -7,7 +7,11 @@ asset=${3:?usage: update-aur-metadata.sh <pkgver> <pkgrel> <asset>}
 checksum=$(sha256sum "$asset" | awk '{print $1}')
 
 perl -0pi -e "s/^pkgver=.*/pkgver=${pkgver}/m; s/^pkgrel=.*/pkgrel=${pkgrel}/m; s/^sha256sums=\('[^']*'\)/sha256sums=('${checksum}')/m" PKGBUILD
-makepkg --printsrcinfo > .SRCINFO
+
+if [[ ${EUID} -eq 0 ]]; then
+  su nobody -s /bin/sh -c 'makepkg --printsrcinfo' > .SRCINFO
+else
+  makepkg --printsrcinfo > .SRCINFO
+fi
 
 printf 'Updated PKGBUILD and .SRCINFO for %s-%s\n' "$pkgver" "$pkgrel"
-
